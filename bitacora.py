adeb607d9c423152f20d4c43bf6bfe9cb7446607
xkraho94x <kraho94@gmail.com>
2026-07-01 19:11:02 -0600

    Terminando el enfoque peligroso

--- bitacora.py
+++ bitacora.py
@@ -1,4 +1,6 @@
 #Enfoque peligroso
 archivo = open("datos.txt", "w")
+#write() es un metodo que permite escribir en un archivo de texto
 archivo.write("Hola mundo, esttoy guardando datos en un archivo de texto")
 archivo.close()
+#write archivo.close() es un metodo que permite cerrar un archivo de texto
